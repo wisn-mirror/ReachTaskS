322e18ab092b141cd2f28a16184ed51f03b7b592
Wisn <wuyishun_kmk@outlook.com>
2018-01-29 10:08:26 +0800

    add dynamic and imagevideofile

--- src/main/sql/reachtask.sql
+++ src/main/sql/reachtask.sql
@@ -21,10 +21,30 @@ CREATE TABLE `rtmessage` (
   `targetuserid` bigint(20) NOT NULL  COMMENT '接收者用户ID',
   `messagetype` int(4) NOT NULL COMMENT '消息类型',
   `status` int(2) NOT NULL COMMENT '消息状态',
-  `content` varchar(1000) NOT NULL COMMENT '手机号',
+  `content` varchar(1000) NOT NULL COMMENT '消息内容',
   `createtime` bigint(13) NOT NULL COMMENT '创建时间',
   `receivetime` bigint(13) NOT NULL COMMENT '接收时间',
   PRIMARY KEY (`messageid`)
+) ENGINE=InnoDB AUTO_INCREMENT=10000 DEFAULT CHARSET=utf8 COMMENT='消息表';
+
+CREATE TABLE `imagevideofile` (
+  `fileid` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '图片id',
+  `status` int(2) NOT NULL COMMENT '图片状态',
+  `imagepath` varchar(1000) NOT NULL COMMENT '图片路径',
+  `createtime` bigint(13) NOT NULL COMMENT '创建时间',
+  PRIMARY KEY (`fileid`)
+) ENGINE=InnoDB AUTO_INCREMENT=10000 DEFAULT CHARSET=utf8 COMMENT='消息表';
+
+CREATE TABLE `dynamic` (
+  `dynamicid` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '动态id',
+  `userid` bigint(20) NOT NULL  COMMENT '发送者用户ID',
+  `status` int(2) NOT NULL COMMENT '图片状态',
+  `content` varchar(1000) COMMENT '动态内容',
+  `imageids` varchar(220) COMMENT '图片ids',
+  `videopath` varchar(220)  COMMENT '视频ids',
+  `location` varchar(20)  COMMENT '地址',
+  `createtime` bigint(13) NOT NULL COMMENT '创建时间',
+  PRIMARY KEY (`dynamicid`)
 ) ENGINE=InnoDB AUTO_INCREMENT=10000 DEFAULT CHARSET=utf8 COMMENT='消息表';
 
 
